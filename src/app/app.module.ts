61a46c8f0b5e5b150468eff27b1d614063ff9683
sumitkr1992 <sumit.kr1992@gmail.com>
2019-05-22 22:37:45 +0530

    Signed-off-by: sumitkr1992 <sumit.kr1992@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListEmployeesComponent } from './employees/list-employees.component';
 import { CreateEmployeeComponent } from './employees/create-employee.component';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { CreateEmployeeComponent } from './employees/create-employee.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BsDatepickerModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Department } from '../model/department.model';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+import { Employee } from '../model/employee.model'; 
 
 @Component({
   selector: 'app-create-employee',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateEmployeeComponent implements OnInit {
 
-  constructor() { }
+  previewPhoto = false; 
+  datePickerConfig: Partial<BsDatepickerConfig>;
+  employee: Employee = {
+    id: null,
+    name: null,
+    gender: null,
+    contactPreference: null,
+    phoneNumber: null,
+    email: null,
+    dateOfBirth: null,
+    department: '-1',
+    isActive: null,
+    photoPath: null
+  }; 
+  departments: Department[] = [
+    { id: 1, name: 'Help Desk' },
+    { id: 2, name: 'HR' },
+    { id: 3, name: 'IT' },
+    { id: 4, name: 'Payroll' }
+  ];
+  
+  constructor() { 
+    this.datePickerConfig = Object.assign({}, { containerClass: 'theme-dark-blue', showWeekNumbers: false, dateInputFormat: 'DD/MM/YYYY' });
+   }
+
+   togglePhotoPreview() {
+    this.previewPhoto = !this.previewPhoto;
+  } 
 
   ngOnInit() {
   }
+  saveEmployee(newEmployee: Employee): void {
+    console.log(newEmployee);
+  } 
 
 }
